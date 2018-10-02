c808822fe02156d714cf9491c383a4acb75c75a8
teresa myers <teresamarie04@gmail.com>
2018-10-02 03:34:26 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  elsif condition
-     int % 5 == 0
+  elsif int % 5 == 0
     "Buzz"
-  else int % 15 == 0
+  elsif int % 15 == 0
     "FizzBuzz"
   end
 
